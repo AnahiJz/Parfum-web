8836e71a1a592c4d26fd77f5f5f466546077272f
Anahi <djassojimenez@gmail.com>
2026-03-03 16:01:48 -0600

    Solucion error de servidor

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'shortline.proxy.rlwy.net',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,
+    // Aquí añadimos tu contraseña de vuelta como respaldo:
+    password: process.env.DB_PASSWORD || 'mjwfMXWdctSyvlUjQSkeUNZXmCgimGaZ', 
     database: process.env.DB_NAME || 'railway',
     port: process.env.DB_PORT || 59357,
     waitForConnections: true,
