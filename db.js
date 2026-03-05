0e7a3a23683f2ce07f78383b8b1b282689ef91e7
Anahi <djassojimenez@gmail.com>
2026-03-05 17:12:18 -0600

    Corrección de servidor

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'shortline.proxy.rlwy.net',
+    host: process.env.DB_HOST || 'mysql.railway.internal',
     user: process.env.DB_USER || 'root',
-    // Aquí añadimos tu contraseña de vuelta como respaldo:
-    password: process.env.DB_PASSWORD || 'mjwfMXWdctSyvlUjQSkeUNZXmCgimGaZ', 
+    password: process.env.DB_PASSWORD || 'mjwfMXWdctSyvlUjQSkeUNZXmCgimGaZ',
     database: process.env.DB_NAME || 'railway',
-    port: process.env.DB_PORT || 59357,
+    port: process.env.DB_PORT || 3306,
+    ssl: { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
