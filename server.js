a76c3b2de9e5d57ef1fd3845f84207152daed7a4
Anahi <djassojimenez@gmail.com>
2026-03-17 22:36:55 -0600

    Entorno ethereal para tickets

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
